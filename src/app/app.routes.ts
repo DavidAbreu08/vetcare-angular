b94be792a7eaf14852a8d1dd2ed80b0a12106096
David Abreu <davidalexandre.abreu@gmail.com>
2025-02-09 18:18:15 +0000

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'home'
+    },
+    {
+        path: 'home',
+        loadComponent: () => import('./modules/home/home.component').then(component => component.HomeComponent)
+    },
+    {
+        path: 'login',
+        loadComponent: () => import('./modules/auth/auth.component').then(component => component.AuthComponent)
+    }
+];
